5cea71d95772581c858a9bba587740f0555ec716
Your Name Herekabaim <eric@twylah.com>
2012-12-31 16:31:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '51907b7e15033de2ec3b94447e16bb6de6e5a20f1006ba03da341be488ef103de2621942f9cbf0f98d429c6e6a3b769255df6b536d198e3c02f1c7bba26130ff'
